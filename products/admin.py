a98dc41a265575e0859cee82f046b0126ce72138
Muhammad Shariq Shafiq <mshariq.official29@gmail.com>
2025-04-09 12:08:37 +0000

    id added in category admin panel

--- products/admin.py
+++ products/admin.py
@@ -3,10 +3,11 @@ from import_export.admin import ExportMixin, ImportExportModelAdmin
 from .models import ProductType, Product, ProductCharacteristic, Category
 from modeltranslation.admin import TranslationAdmin
 
+
 @admin.register(ProductType)
 class ProductTypeAdmin(TranslationAdmin):
     list_display = ('id', 'name')
-    search_fields = ('name',)
+    search_fields = ('name', )
 
 
 class ProductCharacteristicInline(admin.TabularInline):
@@ -16,14 +17,17 @@ class ProductCharacteristicInline(admin.TabularInline):
 
 @admin.register(Product)
 class ProductAdmin(ImportExportModelAdmin, TranslationAdmin):
-    list_display = ('name', 'brand', 'price', 'discount_price', 'published_date', 'product_type', 'category', 'discount_percentage')  
-    list_filter = ('brand', 'product_type', 'category') 
+    list_display = ('name', 'brand', 'price', 'discount_price',
+                    'published_date', 'product_type', 'category',
+                    'discount_percentage')
+    list_filter = ('brand', 'product_type', 'category')
     search_fields = ('name', 'brand', 'description')
-    prepopulated_fields = {'slug': ('name',)}
-    inlines = [ProductCharacteristicInline] 
+    prepopulated_fields = {'slug': ('name', )}
+    inlines = [ProductCharacteristicInline]
 
     def discount_percentage(self, obj):
         return f"{obj.discount_percentage():.2f}%"
+
     discount_percentage.short_description = "Discount %"
 
 
@@ -31,14 +35,12 @@ class ProductAdmin(ImportExportModelAdmin, TranslationAdmin):
 class ProductCharacteristicAdmin(TranslationAdmin):
     list_display = ('product', 'name', 'value')
     search_fields = ('name', 'value')
-    list_filter = ('product',)
+    list_filter = ('product', )
 
 
-@admin.register(Category) 
+@admin.register(Category)
 class CategoryAdmin(TranslationAdmin):
-    list_display = ('name', 'parent', 'slug')  
-    search_fields = ('name',) 
-    prepopulated_fields = {'slug': ('name',)}  
-    list_filter = ('parent',)  
-
-
+    list_display = ('id', 'name', 'parent', 'slug')
+    search_fields = ('name', )
+    prepopulated_fields = {'slug': ('name', )}
+    list_filter = ('parent', )
